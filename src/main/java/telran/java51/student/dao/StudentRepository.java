cc6e67c28044dbc53f4725274e728b99152289f0
jonathankrav <jonathankrav@localhost>
2024-02-11 20:07:40 +0200

    First commit

---
+++ src/main/java/telran/java51/student/dao/StudentRepository.java
@@ -0,0 +1,25 @@
+package telran.java51.student.dao;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import telran.java51.student.model.Student;
+
+
+public interface StudentRepository extends CrudRepository<Student, Integer>{
+
+	Stream<Student> getAllBy();
+	
+	Stream<Student> findByNameIgnoreCase(String name);
+	
+	long countByNameInIgnoreCase(Set<String> names);
+	
+//	@Query("{\"scores.Math\": {$gte: 90}}")
+//	Stream <Student> findByExamAndScoresGreaterThan(String exam, int score);
+	
+	@Query("{\"scores.?0\": {$gt: ?1}}")
+	Stream <Student> findByExamAndScoresGreaterThan(String exam, int score);
+}
